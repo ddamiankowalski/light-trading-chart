131656e9c4a7c4df7d841f49d5bef800010569ae
Damian <dkowalski.1997@gmail.com>
2023-08-19 00:07:57 +0200

    added typescript configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'LightTradingChart'
     })
   ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   devtool: 'inline-source-map',
     devServer: {
     static: './dist',
@@ -17,6 +26,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
   optimization: {
     runtimeChunk: 'single',
