976c4e1c1971f9c3a09f26be2f3200a2c8838f7b
Damian <dkowalski.1997@gmail.com>
2023-08-19 00:04:09 +0200

    initial webpack configuratin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'LightTradingChart'
+    })
+  ],
+  devtool: 'inline-source-map',
+    devServer: {
+    static: './dist',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
