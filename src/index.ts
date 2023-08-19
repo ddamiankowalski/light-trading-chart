74706597d695c5dd6fa49314ac47a184e0a8be56
Damian <dkowalski.1997@gmail.com>
2023-08-19 23:01:41 +0200

    added npmignore

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import './styles.css';
 import { createChart } from './api/create-chart';
 
-const sampleData = [{ y: 123 }, { y: 220 }, { y: 12 }, { y: 125 }, { y: 600 }, { y: 20 }, { y: 330 }];
-
 const chart = createChart(document.getElementById('chart-container') as HTMLElement);
 chart.setData([
   {
