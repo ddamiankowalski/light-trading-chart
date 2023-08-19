e1f66e801ab0363243aafcf42aaba4a68cb73113
Damian <dkowalski.1997@gmail.com>
2023-08-19 02:02:58 +0200

    added chartcomponent

--- src/components/chart.ts
+++ src/components/chart.ts
@@ -1,10 +1,17 @@
 export class ChartComponent {
+  private _observer: ResizeObserver;
+  private _width: number = 0;
+  private _height: number = 0;
+
   constructor(private _container: HTMLElement) {
     this._createComponent();
+    this._observer = this._setResizeObserver();
+    this._observer.observe(this._container);
   }
 
   private _createComponent(): void {
     const div = document.createElement('div');
+    div.classList.add('light-trading-chart__wrapper');
     this.setStyleProperties(div);
     this._container.appendChild(div);
   }
@@ -12,5 +19,14 @@ export class ChartComponent {
   private setStyleProperties(div: HTMLDivElement): void {
     div.style.width = '100%';
     div.style.height = '100%';
+  }
+
+  private _setResizeObserver(): ResizeObserver {
+    return new ResizeObserver((entries) => {
+      const [e] = entries;
+      const { width, height } = e.contentRect;
+      this._width = width;
+      this._height = height;
+    });
   }
 }
