24b68ec6110040c38ce3d9e03a1358f13a809f0a
Damian <dkowalski.1997@gmail.com>
2023-10-06 21:43:26 +0200

    deleted unnecessary model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,77 @@
-export * from './api/create-chart';
+import { createChart } from './api/create-chart';
+export * as s from './api/create-chart';
+
+const chart = createChart(document.getElementById('chart-container'));
+console.log(chart);
+
+chart.setData([
+  {
+    y: 3.017
+  },
+  {
+    y: 3.021
+  },
+  {
+    y: 3.054
+  },
+  {
+    y: 3.052
+  },
+  {
+    y: 3.05
+  },
+  {
+    y: 3.06
+  },
+  {
+    y: 3.053
+  },
+  {
+    y: 3.01
+  },
+  {
+    y: 3.065
+  },
+  {
+    y: 3.069
+  },
+  {
+    y: 3.07
+  },
+  {
+    y: 3.07
+  },
+  {
+    y: 3.068
+  },
+  {
+    y: 3.067
+  },
+  {
+    y: 3.079
+  },
+  {
+    y: 3.091
+  },
+  {
+    y: 3.075
+  },
+  {
+    y: 3.098
+  },
+  {
+    y: 3.1
+  },
+  {
+    y: 3.1
+  },
+  {
+    y: 3.092
+  },
+  {
+    y: 3.097
+  },
+  {
+    y: 3.107
+  }
+]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
-  mode: 'production',
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, '_bundles'),
     filename: '[name].js',
@@ -21,12 +22,17 @@ module.exports = {
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
   devtool: 'source-map',
-    devServer: {
-      static: './dist',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
   },
   resolve: {
-    extensions: ['.ts'],
+    extensions: [".wasm", ".ts", ".tsx", ".mjs", ".cjs", ".js", ".json"],
   },
   optimization: {
     runtimeChunk: 'single',
