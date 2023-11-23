5891e76fc1c15fd61910abb4f9b571f0e664156a
Damian <dkowalski.1997@gmail.com>
2023-11-23 12:34:50 +0100

    added separators

--- src/renderer/time-scale.ts
+++ src/renderer/time-scale.ts
@@ -21,20 +21,28 @@ export class TimeScaleRenderer {
     const colDist = this._view.width / (colQuantity - 1);
 
     const childElements = []
+    let prevX = null;
 
     for (let i = 0; i < colQuantity; i++) {
+      const currX = i * colDist;
+
+      if (prevX !== null && (currX - prevX) < 50) {
+        continue;
+      }
+
       const element = document.createElementNS("http://www.w3.org/2000/svg", "text");
       var textNode = document.createTextNode(new Date(this._view.timestamps[i]).toLocaleDateString('en-GB'));
       element.appendChild(textNode);
-      element.setAttribute("x", (i * colDist).toString());
+      element.setAttribute("x", (currX).toString());
       element.setAttribute("y", y.toString());
       element.setAttribute('fill', 'rgb(151, 159, 181)');
       element.classList.add("light-trading-chart__text");
       childElements.push(element);
       this._svgContainer.append(element);
 
-      element.style.transformOrigin = `${i * colDist}px`;
+      element.style.transformOrigin = `${currX}px`;
       element.style.transform = 'translateY(2.5rem) translateX(0.25rem) rotate(-60deg)';
+      prevX = currX;
     }
   }
 
